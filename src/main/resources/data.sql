941e94c049ea6aa52e5767e6d60efb93d1baa21f
EmJeyDev <emjey@dev.com>
2025-04-09 14:16:35 +0330

    added h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, first_name, last_name, username, email, birthdate)
+VALUES
+    (random_uuid(), 'John', 'Doe', 'johndoe', 'john@example.com', '1990-01-01'),
+    (random_uuid(), 'Jane', 'Smith', 'janesmith', 'jane@example.com', '1985-05-15');
